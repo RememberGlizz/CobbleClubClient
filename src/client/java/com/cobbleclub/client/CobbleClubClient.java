d59ec5b603ad1bbb01ac1ad2736b8255f46f7f8f
CobbleClub <147172246+RememberGlizz@users.noreply.github.com>
2026-09-20 18:50:13 -0400

    Initialize CobbleClub Featherboard HUD

--- src/client/java/com/cobbleclub/client/CobbleClubClient.java
+++ src/client/java/com/cobbleclub/client/CobbleClubClient.java
@@ -24,6 +24,7 @@ package com.cobbleclub.client;
 
 import com.cobbleclub.client.battle.BattleClientInit;
 import com.cobbleclub.client.claims.ClaimsNetworking;
+import com.cobbleclub.client.hud.FeatherboardHud;
 import com.cobbleclub.client.crate.CratePreviewNetworking;
 import com.cobbleclub.client.dashboard.DashboardNetworking;
 import com.cobbleclub.client.gearpreview.GearPreviewNetworking;
@@ -79,6 +80,7 @@ implements ClientModInitializer {
         CratePreviewNetworking.init();
         TagsNetworking.init();
         ClaimsNetworking.init();
+        FeatherboardHud.init();
         DashboardNetworking.init();
         KitsNetworking.init();
         ClientModHandshake.init();
