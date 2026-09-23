abd99839fe9b53572d8e44724ae6fba799b800d4
CobbleClub <147172246+RememberGlizz@users.noreply.github.com>
2026-09-23 02:06:46 -0400

    Apply neutral premium Minecraft UI styling

--- src/client/java/com/cobbleclub/client/ui/ClubPalette.java
+++ src/client/java/com/cobbleclub/client/ui/ClubPalette.java
@@ -1,37 +1,35 @@
-/*
- * Decompiled with CFR 0.152.
- * 
- * Could not load the following classes:
- *  net.fabricmc.api.EnvType
- *  net.fabricmc.api.Environment
- */
 package com.cobbleclub.client.ui;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-@Environment(value=EnvType.CLIENT)
+/**
+ * Neutral Minecraft-inspired CobbleClub palette.
+ *
+ * Layout geometry intentionally lives in the screens; this class only owns color.
+ */
+@Environment(EnvType.CLIENT)
 public final class ClubPalette {
-    public static final int PANEL_TOP = -199614136;
-    public static final int PANEL_BOTTOM = -200601562;
-    public static final int PANEL_BORDER_OUT = -16447985;
-    public static final int PANEL_BORDER_IN = -13747610;
-    public static final int HEADER_TOP = -14405538;
-    public static final int HEADER_BOTTOM = -15459782;
-    public static final int ACCENT = -6467875;
-    public static final int TITLE_COLOR = -2053377;
-    public static final int MUTED_TEXT = -7035976;
-    public static final int POSITIVE = -12474273;
-    public static final int WARNING = -2054081;
-    public static final int NEGATIVE = -2734768;
-    public static final int CONTENT_BG = -15723477;
-    public static final int ROW_BASE = -15064506;
-    public static final int ROW_HOVER = -14405546;
-    public static final int SCROLL_TRACK = -15986650;
-    public static final int SCROLL_THUMB = -10862962;
+    public static final int PANEL_TOP = 0xF4484848;
+    public static final int PANEL_BOTTOM = 0xF4323232;
+    public static final int PANEL_BORDER_OUT = 0xFF111111;
+    public static final int PANEL_BORDER_IN = 0xFF8E8E8E;
+    public static final int HEADER_TOP = 0xFF5A5A5A;
+    public static final int HEADER_BOTTOM = 0xFF3F3F3F;
+    public static final int ACCENT = 0xFFBDBDBD;
+    public static final int TITLE_COLOR = 0xFFF4F4F4;
+    public static final int MUTED_TEXT = 0xFFB0B0B0;
+    public static final int POSITIVE = 0xFF41A85F;
+    public static final int WARNING = 0xFFE0A83F;
+    public static final int NEGATIVE = 0xFFD64550;
+    public static final int CONTENT_BG = 0xFF252525;
+    public static final int ROW_BASE = 0xFF333333;
+    public static final int ROW_HOVER = 0xFF474747;
+    public static final int SCROLL_TRACK = 0xFF202020;
+    public static final int SCROLL_THUMB = 0xFF777777;
+    public static final int TEXT = 0xFFE5E5E5;
     public static final int SHADOW = 0x55000000;
 
     private ClubPalette() {
     }
 }
-
